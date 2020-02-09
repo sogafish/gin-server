44b8c28258553f54cb02970a50de02ab158654f9
sogafish <k.sogafish@gmail.com>
2020-02-09 23:34:36 +0900

    :construction:

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
+	data := "aaa"
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+		// ctx.HTML(200, "index.html", gin.H{})
 	})
 
 	router.Run()
