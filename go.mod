99a3e7ec73534b634ca711a02d329d67efc8e71a
sogafish <k.sogafish@gmail.com>
2020-02-10 13:41:05 +0900

    :hammer:

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gin-server
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/jinzhu/gorm v1.9.12
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+)
--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+// func dbInit() {
+// 	db, err := gorm.Open("sqlite3", "test.sqlite3")
+
+// 	if err != nil {
+// 		panic("gormOpen Error")
+// 	}
+
+// 	db.Create(&Todo{Text: text, Status: status})
+
+// 	defer db.Close()
+// }
 
 func main() {
 	router := gin.Default()
@@ -9,7 +31,6 @@ func main() {
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{"data": data})
-		// ctx.HTML(200, "index.html", gin.H{})
 	})
 
 	router.Run()
